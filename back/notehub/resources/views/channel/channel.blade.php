b6d4021bb8276bb3e4050a85c9185004c76096e2
megumaKeisukeYamashita <19yamashita15@gmail.com>
2016-12-07 01:18:54 +0900

    channelを整えた

--- back/notehub/resources/views/channel/channel.blade.php
+++ back/notehub/resources/views/channel/channel.blade.php
@@ -2,44 +2,58 @@
 
 @section('content')
 
-<div class="col-md-8 col-md-offset-2">
-  <div class="panel panel-default">
-    <div class="panel-body">
-    <div class = "make-all-channel">
-      <table class="table">
-        <thead>
-        </thead>
-          <tr>
-            <th>授業コード</th>
-            <th>講義名</th>
-            <th>担当教員</th>
-            <th>出席人数</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>11001</th>
-            <td>相対論</td>
-            <td>大川湯川</td>
-            <td>30</td>
-          </tr>
-          <tr>
-            <th>10001</th>
-            <td>有機合成化学</td>
-            <td>満島昂佑</td>
-            <td>90</td>
-          </tr>
-          <tr>
-            <th>00111</th>
-            <td>比較思想</td>
-            <td>氷川学</td>
-            <td>50</td>
-          </tr>
-        </tbody>
-        </thead>
-      </table>
-    </div>
-  </div>
-  </div>
-  </div>
+<div class="table-responsive">
+  <table class="table  table-hover">
+    <thead>
+      <tr>
+      <th class="col-md-1">授業コード</th>
+      <th class="col-md-4">講義名</th>
+      <th class="col-md-2">担当教員</th>
+      <th class="col-md-2">教室</th>
+      <th class="col-md-1">入退室</th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>41024</td>
+        <td>有機合成化学</td>
+        <td>近藤検事</td>
+        <td>1108教室</td>
+        <td><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+    </tr>
+    <tr>
+        <td>41111</td>
+        <td>社会国際論</td>
+        <td>氷川学</td>
+        <td>154教室</td>
+        <td><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+    </tr>
+    <tr>
+        <td>31124</td>
+        <td>植物物理学（文科生）</td>
+        <td>三宅忍</td>
+        <td>511教室</td>
+        <td><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+    </tr>
+    <tr>
+        <td>41024</td>
+        <td>有機合成化学</td>
+        <td>近藤検事</td>
+        <td>1108教室</td>
+        <td><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+    </tr>
+    <tr>
+        <td>40113</td>
+        <td>相対論</td>
+        <td>黒川ジャック</td>
+        <td>900教室</td>
+        <td><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></td>
+    </tr>
+  </tbody>
+  </table>
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
 @endsection
