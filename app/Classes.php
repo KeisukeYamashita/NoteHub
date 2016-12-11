75cb0a2281690adac62156c69baefc3ff6093b8a
Masaya Hayashi <mcdonalds.only@gmail.com>
2016-12-11 17:38:00 +0900

    Classesモデルを作成

---
+++ app/Classes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Classes extends Model
+{
+  protected $table = 'classes';
+  protected $primaryKey = 'classid';
+
+  protected $fillable = [
+    'classid',
+    'className',
+    'teacherName',
+    'roomNumber',
+  ];
+  
+}
